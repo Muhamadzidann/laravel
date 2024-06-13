6de66f2c9e6d16e1ebcfba3cd2f8607875d00b3d
Muhamadzidann <muhamadzidan@smkwikrama.sch.id>
2024-06-13 11:38:29 +0700

    upload pertama

---
+++ resources/views/rap.blade.php
@@ -0,0 +1,448 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>portofolio An</title>
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+</head>
+<body>
+    <header>
+        <a href="#" class="logo">ziiDun</a>
+
+        <div class="bx bx-menu" id="menu-icon"></div>
+
+        <ul class="navlist">
+            <li><a href="#"></a></li>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <!--<li><a href="#services">Services</a></li>-->
+            <!-- <li><a href="#services">Skills</a></li> -->
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+
+        <div class="top-btnn">
+            <a href="#" class="h-btn">biodata</a>
+        </div>
+
+    </header>
+
+    <section class="hero" id="home">
+        <div class="main-content" data-aos="fade-in">
+            <h4>Helloo guys</h4>
+            <h1>Nama saya <span>Muhamad zidan</span></h1>
+            <p>saya lahir di bogor pada tanggal 1 april 2008, saya anak pertama, & saya punya hobi main sepakbola.</p>
+            <div class="social">
+                <a href="https://www.instagram.com/xixcivinimeessi/" target="_blank"><i class="ri-instagram-fill"></i></a>
+                <a href="tel:+62856940236"><i class="ri-whatsapp-fill"></i></a>
+                <a href="mailto:muhamadzidan@smkwikrama.sch.id"><i class="ri-mail-open-fill"></i>></a>
+            </div>
+
+            <div class="main-btn">
+                <a href="#about" class="btn">Me</a>
+                <a href="#contact" class="btn btn2">Contact Me</a>
+            </div>
+        </div>
+    </section>
+
+    <section class="about" id="about">
+        <div class="about-img" data-aos="zoom-in-down">
+            <img src="asset/0-komik-batman.jpg">
+        </div>
+        <div class="about-text" data-aos="zoom-in-up">
+            <h2>Saya calon<span> Front-end <br> & back-end develover</span>
+            </h2>
+            <div class="exp-area">
+                <p class="exp"></p>
+                <p class="exp">
+                    spesialis :
+                    <span>Front-end</span>
+                </p>
+                <p class="exp">
+                    Tempat tinggal :
+                    <span>Jl jend ahmad yani blk 46, Bogor.</span>
+                </p>
+                <p class="exp">
+                    Email :
+                    <span>muhamadzidan@smkwikrama.sch.id</span>
+                </p>
+                <p class="exp">
+                    No.telepon :
+                    <span>0856-9480-0236</span>
+                </p>
+            </div>
+            <a href="#portofolio" class="btn">project</a>
+        </div>
+    </section>
+
+    <!--<section class="skil" id="skil">
+            <div class="skill"> 
+                <div class="outer">
+                    <div class="inner">
+                        <div id="number">
+                            HTML
+                            <br>
+                             (40%)
+                        </div>
+                    </div>
+                </div>
+
+                <br>
+
+                <svg xmlns="https://www.w3.org/2000/svg" version="1.1" width="350px" height="350px">
+                    <defs>
+                        <linearGradient id="Gradientcolor">
+                            <stop offset="0%" stop-color="aqua"/>
+                            <stop offset="100%" stop-color="black"/>
+                        </linearGradient>
+                    </defs>
+                    <circle cx="175" cy="175" r="165" stroke-linecap="round"/>
+                </svg>
+            </div> 
+            
+            <div class="skill"> 
+                <div class="outer">
+                    <div class="inner">
+                        <div id="number">
+                             CSS
+                            <br>
+                            (20%)
+                        </div>
+                    </div>
+                </div>
+
+                <svg xmlns="https://www.w3.org/2000/svg" version="1.1" width="350px" height="350px">
+                    <defs>
+                        <linearGradient id="Gradientcolor">
+                            <stop offset="0%" stop-color="aqua"/>
+                            <stop offset="100%" stop-color="black"/>
+                        </linearGradient>
+                    </defs>
+                    <circle cx="125" cy="125" r="115" stroke-linecap="round"/>
+                </svg>
+            </div>
+
+            <div class="skill"> 
+                <div class="outer">
+                    <div class="inner">
+                        <div id="number">
+                            Javascript
+                            <br>
+                             (5%)
+                        </div>
+                    </div>
+                </div>
+
+                <svg xmlns="https://www.w3.org/2000/svg" version="1.1" width="350px" height="350px">
+                    <defs>
+                        <linearGradient id="Gradientcolor">
+                            <stop offset="0%" stop-color="aqua"/>
+                            <stop offset="100%" stop-color="black"/>
+                        </linearGradient>
+                    </defs>
+                    <circle cx="175" cy="175" r="165" stroke-linecap="round"/>
+                </svg>
+            </div>
+    </section>-->
+
+    <!--<section class="di-skills" id="di-skills">
+        <div class="container">
+            <div class="row1">
+                <div class="col-sm-12 col-md-6"->
+                    <div class="mh-skill-inner">
+                        <div class="mh-professional-skill">
+                            <h3> Technical Skills</h3>
+                            <div class="each-skills">
+                                <div class="candidatos">
+                                    <div class="parcial">
+                                        <div class="info">
+                                            <div class="name">HTML</div>
+                                            <div class="percentage-num">40%</div>
+                                        </div>
+                                        <div class="progressBar">
+                                            <div class="percentagem" style="width: 40%;"></div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="candidatos">
+                                    <div class="parcial">
+                                        <div class="info">
+                                            <div class="name">CSS</div>
+                                            <div class="percentage-num">20%</div>
+                                        </div>
+                                        <div class="progressBar">
+                                            <div class="percentagem" style="width: 20%;"></div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="candidatos">
+                                    <div class="parcial">
+                                        <div class="info">
+                                            <div class="name">Javascript</div>
+                                            <div class="percentage-num">5%</div>
+                                        </div>
+                                        <div class="progressBar">
+                                            <div class="percentagem" style="width: 5%;"></div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="candidatos">
+                                    <div class="parcial">
+                                        <div class="info">
+                                            <div class="name">PHP</div>
+                                            <div class="percentage-num">0.5%</div>
+                                        </div>
+                                        <div class="progressBar">
+                                            <div class="percentagem" style="width: 0.5%;"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-12 col-md-6">
+                    <div class="mh-professional-skills">
+                        <h3> Professional Skills</h3>
+                        <ul class="mh-professional-progress">
+                            <li>
+                                <div class="mh-progress mh-progress-circle" data-progress="80"></div>
+                                <div class="pr-skill-name">Komunikasi</div>
+                            </li>
+                            <li>
+                                <div class="mh-progress mh-progress-circle" data-progress="60"></div>
+                                <div class="pr-skill-name">Team work</div>
+                            </li>
+                            <li>
+                                <div class="mh-progress mh-progress-circle" data-progress="50"></div>
+                                <div class="pr-skill-name">Kreativitas</div>
+                            </li>
+                            <li>
+                                <div class="mh-progress mh-progress-circle" data-progress="50"></div>
+                                <div class="pr-skill-name">Keaktifan</div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>-->
+
+    <!-- <section class="services" id="services">
+        <div class="center-text" data-aos="zoom-in-down">
+            <h2>My<span> skills </span></h2>
+        </div>
+        <div class="services-content">
+            <div class="box" data-aos="zoom-in-up">
+                img src="image/s-1.svg">
+                <h3>HTML</h3>
+                <div class="skill"> 
+                    <div class="outer">
+                        <div class="inner">
+                            <div id="number">
+                                <em>HTML</em>
+                                <br>
+                                 <em>(40%)</em>
+                            </div>
+                        </div>
+                    </div>
+    
+                    <br>
+    
+                    <svg xmlns="https://www.w3.org/2000/svg" version="1.1" width="350px" height="350px">
+                        <defs>
+                            <linearGradient id="Gradientcolor">
+                                <stop offset="0%" stop-color="aqua"/>
+                                <stop offset="100%" stop-color="black"/>
+                            </linearGradient>
+                        </defs>
+                        <circle class="circle1" cx="107" cy="107" r="97" stroke-linecap="round"/>
+                    </svg>
+                </div> 
+            </div>
+            <div class="box" data-aos="zoom-in-up">
+                <img src="image/s-2.svg">
+                <h3>CSS</h3>
+                <div class="skill"> 
+                    <div class="outer">
+                        <div class="inner">
+                            <div id="number">
+                                <em>CSS</em>
+                                <br>
+                                 <em>(35%)</em>
+                            </div>
+                        </div>
+                    </div>
+    
+                    <br>
+    
+                    <svg xmlns="https://www.w3.org/2000/svg" version="1.1" width="350px" height="350px">
+                        <defs>
+                            <linearGradient id="Gradientcolor">
+                                <stop offset="0%" stop-color="aqua"/>
+                                <stop offset="100%" stop-color="black"/>
+                            </linearGradient>
+                        </defs>
+                        <circle class="circle1" cx="107" cy="107" r="97" stroke-linecap="round"/>
+                    </svg>
+                </div> 
+            </div>
+            <div class="box" data-aos="zoom-in-up">
+                <img src="image/s-3.svg">
+                <h3>Javascript</h3>
+                <div class="skill"> 
+                    <div class="outer">
+                        <div class="inner">
+                            <div id="number">
+                                <em>Javascript</em>
+                                <br>
+                                 <em>(3%)</em>
+                            </div>
+                        </div>
+                    </div>
+    
+                    <br>
+    
+                    <svg xmlns="https://www.w3.org/2000/svg" version="1.1" width="350px" height="350px">
+                        <defs>
+                            <linearGradient id="Gradientcolor">
+                                <stop offset="0%" stop-color="aqua"/>
+                                <stop offset="100%" stop-color="black"/>
+                            </linearGradient>
+                        </defs>
+                        <circle class="circle1" cx="107" cy="107" r="97" stroke-linecap="round"/>
+                    </svg>
+                </div> 
+            </div>
+        </div>
+    </section>
+
+    <section class="portofolio" id="portofolio">
+        <div class="center-text" data-aos="zoom-in-down">
+            <h2>My<span> Portofolio </span></h2>
+        </div>
+        <div class="portofolio-content" data-aos="zoom-in-up">
+            <div class="row">
+                <img src="image/nice.png" class="bisa">
+                <div class="main-row">
+                    <div class="row-text">
+                        <h5><a href="file:///D:/Coding%20dudul/porto.html#home" target="_blank"><em>web portofolio</em></a></h5>
+                    </div>
+                    <div class="row-icon">
+                        <i class="ri-github-fill"></i>
+                    </div>
+                    <h4>project 1</h4>
+                </div>
+            </div>
+            <div class="row">
+                <img src="image/bg 3.png" class="bisa">
+                <div class="main-row">
+                    <div class="row-text">
+                        <h5><a href="file:///D:/Coding%20dudul/list/list.html" target="_blank"><em>web list</em></a></h5>
+                    </div>
+                    <div class="row-icon">
+                        <i class="ri-github-fill"></i>
+                    </div>
+                    <h4>2</h4>
+                </div>
+            </div>
+            <div class="row">
+                <img src="image/proyek 1.png" class="bisa">
+                <div class="main-row">
+                    <div class="row-text">
+                        <h5><a href="file:///D:/Coding%20dudul/project/frontend/profile.html" target="_blank"><em>web edukasi</em></a></h5>
+                    </div>
+                    <div class="row-icon">
+                        <i class="ri-github-fill"></i>
+                    </div>
+                    <h4>3<h4>
+                </div>
+            </div>
+            <div class="row">
+                <img src="image/contoh.png" class="bisa">
+                <div class="main-row">
+                    <div class="row-text">
+                        <h5><a href="file:///D:/Coding%20dudul/tes/table.html" target="_blank">contoh</a></h5>
+                    </div>
+                    <div class="row-icon">
+                        <i class="ri-github-fill"></i>
+                    </div>
+                    <h4>4</h4>
+                </div>
+            </div>
+            <div class="row">
+                <img src="image/contoh.png" class="bisa">
+                <div class="main-row">
+                    <div class="row-text">
+                        <h5><a href="file:///D:/Coding%20dudul/tes/table.html" target="_blank">contoh</a></h5>
+                    </div>
+                    <div class="row-icon">
+                        <i class="ri-links-fill"></i>
+                    </div>
+                    <h4>muhamad zidan</h4>
+                </div>
+            </div>
+            <div class="row">
+                <img src="image/contoh.png" class="bisa">
+                <div class="main-row">
+                    <div class="row-text">
+                        <h5><a href="file:///D:/Coding%20dudul/tes/table.html" target="_blank">contoh</a></h5>
+                    </div>
+                    <div class="row-icon">
+                        <i class="ri-github-fill"></i>
+                    </div>
+                    <h4>My list</h4>
+                </div>
+            </div>
+        </div>
+    </section> -->
+    
+    <section class="contact" id="contact">
+        <div class="center-text" data-aos="zoom-in-down">
+            <h2>Contact<span> Me </span></h2>
+        </div>
+        <div class="contact-form" data-aos="zoom-in-up">
+            <form action="">
+                <input type="text" placeholder="Your-name" required>
+                <input type="email" placeholder="Email Address.." required>
+                <textarea name="" id="" cols="30" rows="10" placeholder="Write Message Here..." required></textarea>
+                <input type="submit" name="" value="Send Message" class="send-btn">
+            </form>
+        </div>
+    </section>
+
+    <div class="footer">
+        <div class="copyright">
+            <p>&copy; copyright 2k23 muhamadzidan.</p>
+        </div>
+        <a href="#home" class="scroll-top">
+            <i class="ri-arrow-up-double-fill"></i>
+        </a>
+    </div>
+
+    <script src="script.js"></script>
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+      AOS.init({
+        offset: 250,
+        duration: 100,
+      });
+    </script>
+    
+</body>
+</html>
+     
+
+
+
